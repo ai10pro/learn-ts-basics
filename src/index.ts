c3e5831ed3e4cba43819cf7e25269ccf48f3aa39
ai10pro <106215301+ai10pro@users.noreply.github.com>
2024-10-03 10:27:43 +0900

    update src/index.ts and prac01.ts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-console.log('■ prac00.tsの実行');
-import "./prac00";
+// console.log('■ prac00.tsの実行');
+// import "./prac00";
 
 console.log('■ prac01.tsの実行');
 import "./prac01";
--- src/prac01.ts
+++ src/prac01.ts
@@ -1,3 +1,12 @@
 export{};
-const userName: string = "Bob";
-console.log(`Hi ${userName}!`);
+// const userName: string = "Bob";
+// console.log(`Hi ${userName}!`);
+
+let name: string = "TypeScriptの勉強";
+let priority: number = 3;
+
+console.log(name,priority);
+
+// Data型の変数 deadlineの宣言と初期化
+let deadline: Date = new Date(2024, 10, 3, 10, 25);
+console.log(deadline);
