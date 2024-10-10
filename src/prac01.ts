d929941c034ae438ce5a205108f4dd88715a41ca
ai10pro <106215301+ai10pro@users.noreply.github.com>
2024-10-10 09:58:39 +0900

    dayjsの追加

--- src/prac01.ts
+++ src/prac01.ts
@@ -1,9 +1,12 @@
 export {};
-import { date2str } from "./date2str";
+import dayjs from 'dayjs';
+import 'dayjs/locale/ja';
 
+dayjs.locale('ja');
+const dtFmt = 'YYYY/MM/DD(dd) HH:mm';
 const deadline: Date = new Date(2024, 9, 10, 11, 45);
 const createdAt: Date = new Date();
 
-let str = `期限:${date2str(deadline)},
-作成日:${date2str(createdAt)}`;
+const str = `期限 ${dayjs(deadline).format(dtFmt)}` +
+            ` 作成日 ${dayjs(createdAt).format(dtFmt)}`;
 console.log(str);
