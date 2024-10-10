ac58b5d170a5919db0b8739ae1f5c5acb257483a
ai10pro <106215301+ai10pro@users.noreply.github.com>
2024-10-10 09:12:50 +0900

    update prac01.ts

--- src/prac01.ts
+++ src/prac01.ts
@@ -1,3 +1,5 @@
+import { json } from "stream/consumers";
+
 export{};
 // const userName: string = "Bob";
 // console.log(`Hi ${userName}!`);
@@ -6,7 +8,17 @@ let name: string = "TypeScriptの勉強";
 let priority: number = 3;
 
 console.log(name,priority);
+console.log(`タスク名：${name}、優先度：${priority}`);
 
 // Data型の変数 deadlineの宣言と初期化
 let deadline: Date = new Date(2024, 10, 3, 10, 25);
 console.log(deadline);
+
+// オブジェクト定義
+const todo = {
+    name: "TypeScriptの勉強",
+    priority: 3,
+    deadline: new Date(2024, 10, 3, 10, 25)
+}
+console.log(todo);
+console.log(JSON.stringify(todo, null, 2));
