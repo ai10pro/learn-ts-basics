ef50492fc24a3b7461ed11d3e1b877394252eb04
ai10pro <106215301+ai10pro@users.noreply.github.com>
2024-10-17 10:11:50 +0900

    update prac06.ts

--- src/prac06.ts
+++ src/prac06.ts
@@ -1,8 +1,6 @@
-export{};
-
-const grades: number[] = [1, 2, 3, 4, 5];
-const gradeListItems = grades.map((grade: number): string => `<li>${grade}年</li>`
-);
-
-console.log(grades);
-console.log(gradeListItems);
+export {};
+const arr = ["Python", "C言語", "TypeScript", "C#"];
+const arr2 = arr.map((value, index) => { // index の設定
+  return `${index + 1}: ${value}`; // index の参照 (読み取り)
+});
+console.log(arr2);
