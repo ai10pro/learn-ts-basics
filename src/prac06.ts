de02ceacbc24a39727cc6fbf61bd579becb0ee53
ai10pro <106215301+ai10pro@users.noreply.github.com>
2024-10-17 10:00:54 +0900

    update prac06.ts

--- src/prac06.ts
+++ src/prac06.ts
@@ -1,10 +1,8 @@
 export{};
 
-const grades:number[] = [1, 2, 3, 4, 5];
-
-const gradeListItems = grades.map((grade:number):string => {
-    return `<li>${grade}</li>`;
-});
+const grades: number[] = [1, 2, 3, 4, 5];
+const gradeListItems = grades.map((grade: number): string => `<li>${grade}年</li>`
+);
 
 console.log(grades);
 console.log(gradeListItems);
