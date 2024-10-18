65be6502ffa80be0f7d6c46f7f7f6b65ac71655d
ai10pro <106215301+ai10pro@users.noreply.github.com>
2024-10-19 00:10:35 +0900

    update prac08.ts

--- src/prac08.ts
+++ src/prac08.ts
@@ -1,9 +1,7 @@
 export{};
 
 const numArr: number[] = [1, 2, 3, 4, 5, 6];
-const oddArr: number[] = numArr.filter((num) => {
-    return num % 2 !== 0;
-});
+const oddArr: number[] = numArr.filter(num => num % 2 !== 0);
 
 console.log(numArr); // [1, 2, 3, 4, 5, 6]
 console.log(oddArr); // [1, 3, 5]
