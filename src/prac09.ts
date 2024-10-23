83310ed9d44bdb237f2d767728d226a7dcbeebbc
ai10pro <106215301+ai10pro@users.noreply.github.com>
2024-10-24 01:03:33 +0900

    update prac09.ts

--- src/prac09.ts
+++ src/prac09.ts
@@ -6,7 +6,13 @@ const sortedTodos = [...initTodos].sort((a, b) => {
     return a.priority - b.priority;
 }) 
 
+const sortedTodos2 = [...initTodos].sort((a, b) => {
+    return b.priority - a.priority;
+})
+
 console.log("initTodos");
 console.log(JSON.stringify(initTodos, null, 2));
-console.log("sortedTodos");
+console.log("昇順");
 console.log(JSON.stringify(sortedTodos, null, 2));
+console.log("降順");
+console.log(JSON.stringify(sortedTodos2, null, 2));
