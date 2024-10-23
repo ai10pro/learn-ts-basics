14898aca19a47bfdaf8429a9d7dcfa5b2ea75b37
ai10pro <106215301+ai10pro@users.noreply.github.com>
2024-10-24 01:39:37 +0900

    update prac10.ts

--- src/prac10.ts
+++ src/prac10.ts
@@ -1,5 +1,6 @@
 export {};
 import dayjs from "dayjs";
+import { Todo } from "./types";
 import { initTodos } from "./initTodos";
 const dtFmt = "YYYY/MM/DD HH:mm";
 
@@ -12,5 +13,25 @@ const listItemsTodos: string[] = initTodos
       `<li>優先度[${todo.priority}] ${todo.name}` +
       `...期限${dayjs(todo.deadline).format(dtFmt)}</li>`
   );
-console.log(JSON.stringify(initTodos, null, 2)); // 変更操作の影響を受けていない
-console.log(JSON.stringify(listItemsTodos, null, 2));
+
+const deleteTodoById = (todos: Todo[], id: number): Todo[] => {
+  if (!Number.isInteger(id) || id <= 0) {
+    return todos;
+  }
+  return todos.filter((todo) => todo.id !== id);
+};
+
+const stringifyTodos = (todos: Todo[]): string[] =>
+  todos.map(
+    (todo) =>
+      `id=${todo.id} ${todo.name} ` +
+      `期限 ${dayjs(todo.deadline).format(dtFmt)}`
+  );
+
+const todo = [...initTodos];
+const updatedTodos = deleteTodoById(todo, 3);
+
+console.log("削除前");
+console.log(stringifyTodos(todo));
+console.log("削除後");
+console.log(stringifyTodos(updatedTodos));
